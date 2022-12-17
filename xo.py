afa0f7887a0ea686be299b76b3bda2ec7b7ddf8c
alindulceanu <dulceanu.io.andrei@student.utcluj.ro>
2022-12-17 21:49:28 +0200

    Am adaugat buton de reset si o tentativa de timer

--- xo.py
+++ xo.py
@@ -1,11 +1,23 @@
 import tkinter as tk
 import tkinter.messagebox as mb
-
+import time
 
 player = 1
 win_game = False
+g = True
 
- 
+
+def matrix():
+    
+    for i in range(3):
+        for j in range(3):
+            b[i][j] = tk.Button(
+                height = 10,
+                width = 30,
+                command = lambda x = i, y = j: click(x, y)
+            )
+            b[i][j].grid(row = i, column = j)
+
 def click(x, y):
 
     global player
@@ -23,28 +35,49 @@ def click(x, y):
     win()
 
 def win():
-
+    global g
     global win_game
-
+    global winner
     for i in range(3):
         if poz[i][0] == poz[i][1] == poz[i][2] != 0:
             win_game = True
-            winner = mb.showinfo("Felicitari!", poz[i][0]+ " a castigat!")
+            winner.configure(text = poz[i][1] + " a castigat")
+            g = False
             break
 
         elif poz[0][i] == poz[1][i] == poz[2][i] != 0:
             win_game = True
-            winner = mb.showinfo("Felicitari!", poz[i][0]+ " a castigat!")
+            winner.configure(text = poz[1][i] + " a castigat")
+            g = False
             break
 
     if poz[0][0] == poz[2][2] == poz[1][1] != 0:
         win_game = True
-        winner = mb.showinfo("Felicitari!", poz[1][1]+ " a castigat!")
+        winner.configure(text = poz[1][1] + " a castigat")
+        g = False
         
     elif poz[0][2] == poz[1][1] == poz[2][0] != 0:
         win_game = True
-        winner = mb.showinfo("Felicitari!", poz[1][1]+ " a castigat!")
+        winner.configure(text = poz[1][1] + " a castigat")
+        g = False
         
+def reset():
+    global poz
+    global player
+    global win_game
+    global g
+    global start_time
+    poz = [
+     [0 ,0 ,0],
+     [0 ,0 ,0],
+     [0 ,0 ,0]]
+    
+    winner.configure(text = "win")
+    player = 1
+    win_game = False
+    matrix()
+    g = True
+    start_time = time.time()
 
 root = tk.Tk()
 root.title("Tik Tac Toe")
@@ -61,14 +94,23 @@ poz = [
      [0 ,0 ,0]
 ]
 
-for i in range(3):
-    for j in range(3):
-        b[i][j] = tk.Button(
-            text = " ",
-            height = 10,
-            width = 30,
-            command = lambda x = i, y = j: click(x, y)
-        )
-        b[i][j].grid(row = i, column = j)
+matrix()
+
+reset = tk.Button(
+    text = "Reset",
+    command = reset
+)
+reset.grid(row = 3, column = 0)
+winner = tk.Label(text = "win")
+winner.grid(row = 3, column = 1)
+
+start_time = time.time()
+
+while g == True:
+    end_time = time.time()
+    timer = end_time - start_time
+
+timeLabel = tk.Label(text = timer)
+timeLabel.grid(row = 3, column = 2)
 
 root.mainloop()
